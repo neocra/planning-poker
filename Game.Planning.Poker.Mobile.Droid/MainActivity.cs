65b46f26f2595c446cc6042e53fce5cc210c75d0
Aurélien GALTIER <aurelien.galtier@live.fr>
2019-01-14 17:24:27 +0100

    Add first Android Project

---
+++ Game.Planning.Poker.Mobile.Droid/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Game.Planning.Poker.Mobile.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+//            ZXing.Net.Mobile.Forms.iOS.Platform.Init();
+//
+//            Forms.SetFlags("CollectionView_Experimental");
+//            Xamarin.Forms.Forms.Init();
+//
+//            this.LoadApplication(new App(this.LoadKernel));
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
